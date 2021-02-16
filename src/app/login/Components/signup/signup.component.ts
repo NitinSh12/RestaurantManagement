eb962e755c00387f5ce598df872b284dbbba2301
Nitin Sharma <nitinsharma.mtech@gmail.com>
2021-02-16 10:27:24 +0530

    design modification for login and app component

--- src/app/login/Components/signup/signup.component.ts
+++ src/app/login/Components/signup/signup.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SignupComponent implements OnInit {
   Roles: any = ['Admin', 'Author', 'Reader'];
-  
+
   constructor() { }
 
   ngOnInit(): void {
